1424e6626dda2ffca7c3353be08a5f6bceddef3e
Gabriel Rangel <gabrielmrangel7@gmail.com>
2022-01-08 17:49:18 -0300

    Adding network helper for requests

---
+++ scryfall_app/lib/network/net_helper.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  NetworkHelper(this.urlBase);
+
+  final String urlBase;
+
+  /// Request to get data
+  Future getData(String endpoint) async {
+    http.Response response = await http.get(Uri.parse(urlBase + endpoint));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
