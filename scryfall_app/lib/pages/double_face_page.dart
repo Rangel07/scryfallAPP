a24f073396652eb95dcf14cad1cca9c130773a85
Gabriel Rangel <gabrielmrangel7@gmail.com>
2022-01-23 13:28:07 -0300

    Completed page for double faced cards

--- scryfall_app/lib/pages/double_face_page.dart
+++ scryfall_app/lib/pages/double_face_page.dart
@@ -12,6 +12,16 @@ class DoubleFacePage extends StatefulWidget {
 }
 
 class _DoubleFacePageState extends State<DoubleFacePage> {
+  int cardFace = 0;
+
+  void changeFace() {
+    if (cardFace == 0) {
+      cardFace = 1;
+    } else {
+      cardFace = 0;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,35 +35,72 @@ class _DoubleFacePageState extends State<DoubleFacePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Button to flip card
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 125,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(kgrey)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.refresh),
+                      SizedBox(width: 10,),
+                      Text('Flip Card'),
+                    ],
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      changeFace();
+                    });
+                  },
+                ),
+              ),
+            ),
             // Card image in normal size
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Image.network(widget.data['image_uris']['normal']),
+              child: Image.network(
+                  widget.data['card_faces'][cardFace]['image_uris']['normal']),
             ),
             // Box with card type
             Padding(
-              padding: const EdgeInsets.fromLTRB(8,0,8,0),
+              padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
               child: Card(
                 color: kgrey,
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8,0,8,8),
-                  child: Text(widget.data['type_line'], style: ktextCard,),
+                  padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+                  child: Text(
+                    widget.data['card_faces'][cardFace]['type_line'],
+                    style: ktextCard,
+                  ),
                 ),
               ),
             ),
             // Box with the name of this card artist
             Padding(
-              padding: const EdgeInsets.fromLTRB(8,0,8,0),
+              padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
               child: Card(
                 color: kgrey,
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8,0,8,0),
+                  padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Icon(Icons.brush_sharp, color: kwhite,),
-                      Container(width: 8,),
-                      Text("Artist ${widget.data['artist']}", style: ktextCard,),
+                      const Icon(
+                        Icons.brush_sharp,
+                        color: kwhite,
+                      ),
+                      Container(
+                        width: 8,
+                      ),
+                      Text(
+                        "Artist ${widget.data['card_faces'][cardFace]['artist']}",
+                        style: ktextCard,
+                      ),
                     ],
                   ),
                 ),
@@ -61,7 +108,6 @@ class _DoubleFacePageState extends State<DoubleFacePage> {
             ),
             // List with Play Formats and its legalities
             CardLegal(data: widget.data['legalities'])
-            
           ],
         ),
       ),
